f1f137858d69e3976f54de61130f8dad1d1be38e
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-02-16 18:25:12 -0400

    feat: saludar por hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,16 @@
 function saludar(nombre) {
-  alert(`Hola, ${nombre}!`);
+  const hora = new Date().getHours();
+  let saludo;
+
+  if (hora >= 5 && hora < 12) {
+      saludo = "Buenos días";
+  } else if (hora >= 12 && hora < 18) {
+      saludo = "Buenas tardes";
+  } else {
+      saludo = "Buenas noches";
+  }
+
+  alert(`${saludo}, ${nombre}!`);
 }
 
 export default saludar;
