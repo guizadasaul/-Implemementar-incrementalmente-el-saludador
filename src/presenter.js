6a613537cf036f4320dab182ae37c70730820348
guizadasaul <saulguizada@Sauls-MacBook-Pro.local>
2025-02-16 20:06:05 -0400

    feat: saluda en dos idiomas

--- src/presenter.js
+++ src/presenter.js
@@ -3,22 +3,24 @@ import saludar from "./saludador.js";
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector(".saludar-btn");
     const nombreInput = document.querySelector("#nombre");
-    const generoSelector = document.querySelector(".select");
+    const generoSelector = document.querySelector("#genero");
     const edadInput = document.querySelector("#edad");
+    const idiomaSelector = document.querySelector("#idioma");
 
     button.addEventListener("click", () => {
         const nombre = nombreInput.value.trim();
         const genero = generoSelector.value;
         const edad = parseInt (edadInput.value);
+        const idioma = idiomaSelector.value;
 
         if (nombre && edad) {
-            saludar(nombre, genero, edad);
+            saludar(nombre, genero, edad, idioma);
         }else if (!nombre && edad) {
-            alert("por favor introduce tu nombre");
+            alert(idioma === "es" ? "por favor introduce tu nombre" : "please enter your name");
         }else if (!nombre && !edad) {
-            alert("por favor introduce tus datos");
+            alert(idioma === "es" ? "por favor introduce tus datos" : "please enter your data");
         }else if (nombre && !edad) {
-            alert("por favor introduce tu edad");
+            alert(idioma === "es" ? "por favor introduce tu edad" : "please enter your age");
         }
     });
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,22 +1,45 @@
-function saludar(nombre, genero, edad) {
+function saludar(nombre, genero, edad, idioma) {
   const hora = new Date().getHours();
   let saludo;
   
-  if (hora >= 5 && hora < 12) {
-      saludo = "Buenos dias";
-  } else if (hora >= 12 && hora < 18) {
-      saludo = "Buenas tardes";
+  if (idioma === "es") {
+    if (hora >= 5 && hora < 12) {
+        saludo = "Buenos días";
+    } else if (hora >= 12 && hora < 18) {
+        saludo = "Buenas tardes";
+    } else {
+        saludo = "Buenas noches";
+    }
   } else {
-      saludo = "Buenas noches";
+    if (hora >= 5 && hora < 12) {
+        saludo = "Good morning";
+    } else if (hora >= 12 && hora < 18) {
+        saludo = "Good afternoon";
+    } else {
+        saludo = "Good evening";
+    }
   }
-  if(edad >= 31 && genero === "Mujer"){
-    genero = "señora";
-  }else if(edad <31 && genero === "Mujer"){
-    genero = "señorita";
-  }else if(edad >=31 && genero === "Hombre"){
-    genero = "señor";
-  }else if(edad <31 && genero === "Hombre"){
-    genero = "joven";
+
+  if (idioma === "es") {
+    if (edad >= 31 && genero === "Mujer") {
+        genero = "señora";
+    } else if (edad < 31 && genero === "Mujer") {
+        genero = "señorita";
+    } else if (edad >= 31 && genero === "Hombre") {
+        genero = "señor";
+    } else if (edad < 31 && genero === "Hombre") {
+        genero = "joven";
+    }
+  } else {
+    if (edad >= 31 && genero === "Mujer") {
+        genero = "madam";
+    } else if (edad < 31 && genero === "Mujer") {
+        genero = "miss";
+    } else if (edad >= 31 && genero === "Hombre") {
+        genero = "sir";
+    } else if (edad < 31 && genero === "Hombre") {
+        genero = "young";
+    }
   }
 
   alert(`${saludo} ${genero} ${nombre}!`);
